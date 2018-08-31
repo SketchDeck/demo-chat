1f587079635ab1f3106ff5cb8872f822b7e2b621
Jonathan Serle <jdserle@gmail.com>
2018-08-31 18:32:16 +0000

    update libs, add algolia module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { NgAisModule } from 'angular-instantsearch';
 
 
 import { AppComponent } from './app.component';
