fa9f8b1500e66df7cf92801d17ad428c5b676b10
Jonathan Serle <jdserle@gmail.com>
2018-09-03 18:37:43 +0000

    complete angular 6 upgrade

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, filter, catchError, mergeMap } from 'rxjs/operators'
 
 @Injectable()
 export class DataService {
@@ -9,7 +10,13 @@ export class DataService {
 	}
 
 	getUser() {
-		return this.http.get('https://randomuser.me/api/').map( (i) => { return i['results'][0]; } );
+		return this.http.get('https://randomuser.me/api/')
+			.pipe(
+				map((data: any[]) => {
+					console.log('getUser results', data);
+					return data['results'][0];
+				})	
+			);
 	}
 
 }
