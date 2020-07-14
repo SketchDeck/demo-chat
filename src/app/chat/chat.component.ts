288367fd0c77db5fa307b2cb0099884cad767f57
Joe <joe.teibel@sketchdeck.com>
2020-07-14 16:56:56 -0700

    5 to 6

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,86 +1,86 @@
-import { Component, OnInit, AfterViewChecked, Inject, ViewChild, ElementRef } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
 
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/first';
+import {first} from 'rxjs/operators';
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  Inject,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+import { TitleCasePipe } from "@angular/common";
 
-import { Subscription } from 'rxjs/Subscription';
-import * as firebase from 'firebase';
+import { Observable ,  Subscription } from "rxjs";
+import "rxjs/add/operator/first";
+import * as firebase from "firebase";
 
-import { IChat } from '../../model/chat';
-import { DataService } from '../data.service';
+import { IChat } from "../../model/chat";
+import { DataService } from "../data.service";
 
 // class Chat implements IChat {}
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"],
 })
-export class ChatComponent implements OnInit, AfterViewChecked
-{
-  chatCollection: AngularFirestoreCollection<IChat>;
+export class ChatComponent implements OnInit, AfterViewChecked {
   chat$: Observable<IChat[]>;
   model: IChat;
 
   user$: Observable<Object>;
   userObject: Object;
 
-  @ViewChild('chatStream') private container: ElementRef;
+  @ViewChild("chatStream") private container: ElementRef;
 
-  resetModel(){
-  	this.model = {} as IChat;
+  resetModel() {
+    this.model = {} as IChat;
   }
 
   constructor(
-      db: AngularFirestore, 
-      data: DataService) 
-  {
-    this.chatCollection = db.collection<IChat>('chat', ref => ref.orderBy("dateCreated"))
-    this.chat$ = this.chatCollection.valueChanges();
+    // db: AngularFirestore,
+    data: DataService
+  ) {
+    // this.chatCollection = db.collection<IChat>('chat', ref => ref.orderBy("dateCreated"))
+    // this.chat$ = this.chatCollection.valueChanges();
     this.user$ = data.getUser();
 
-    this.chat$.subscribe(() => {this.scrollToBottom()})
+    this.chat$.subscribe(() => {
+      this.scrollToBottom();
+    });
 
     this.resetModel();
   }
 
   scrollToBottom(): void {
-      try {
-        this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
-      } catch(err) { 
-        console.error(err);
-        console.log(this.container);
-      }                 
+    try {
+      this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error(err);
+      console.log(this.container);
     }
+  }
 
   ngOnInit() {
-    this.user$.first().subscribe( (u) => this.userObject = u );
+    this.user$.pipe(first()).subscribe((u) => (this.userObject = u));
     this.scrollToBottom();
   }
 
-  ngAfterViewChecked() {        
-    this.scrollToBottom();        
-  } 
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
 
   onSubmit() {
-
     if (this.model.text != "" && this.model.text != undefined) {
-    	this.model.dateCreated = firebase.firestore.FieldValue.serverTimestamp()
+      this.model.dateCreated = firebase.firestore.FieldValue.serverTimestamp();
       this.model.from = this.userObject;
-      this.chatCollection.add(this.model);
+      // this.chatCollection.add(this.model);
       this.resetModel();
       this.scrollToBottom();
     }
-   
   }
 
-  public tracker(index, item):any {
+  public tracker(index, item): any {
     return item.$id;
   }
-
-  
-
-
 }
